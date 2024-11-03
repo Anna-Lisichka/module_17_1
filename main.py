f221a43065d42d178a56c5bcbf150cb8ebca41c8
Anna-Lisichka <an_na22@mail.ru>
2024-11-03 13:19:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from app.routers import task, user
 
-
-app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
-
+app = FastAPI()
 
 @app.get("/")
 async def welcome():
